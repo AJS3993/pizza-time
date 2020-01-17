192afd754ac38e1041433a7672106c19566004e5
Andrew Smith <ajs3993@gmail.com>
2020-01-17 16:52:14 -0600

    authentication done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
-const NavBar = (props) => {
-  let nav = props.user ?
+const NavBar = props => {
+  let nav = props.user ? (
     <div>
-      <Link to='' className='NavBar-link' onClick={props.handleLogout}>LOG OUT</Link>
+      <Link to="" className="NavBar-link" onClick={props.handlelogout}>
+        LOG OUT
+      </Link>
       &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-      <span className='NavBar-welcome'>WELCOME, {props.user.name}</span>
+      <span className="NavBar-welcome">WELCOME, {props.user.name}</span>
     </div>
-    :
+  ) : (
     <div>
-      <Link to='/login' className='NavBar-link'>LOG IN</Link>
+      <Link to="/login" className="NavBar-link">
+        LOG IN
+      </Link>
       &nbsp;&nbsp;|&nbsp;&nbsp;
-      <Link to='/signup' className='NavBar-link'>SIGN UP</Link>
-    </div>;
-
-  return (
-    <div className='NavBar'>
-      {nav}
+      <Link to="/signup" className="NavBar-link">
+        SIGN UP
+      </Link>
     </div>
   );
+
+  return <div className="NavBar">{nav}</div>;
 };
 
 export default NavBar;
