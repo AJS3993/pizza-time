0a8ae44dfda23d4c838d9be0419f4b88f5f5ff8b
Andrew Smith <ajs3993@gmail.com>
2020-01-23 13:09:09 -0600

    about page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import  {MDBContainer, MDBRow, MDBCol} from "mdbreact";
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
+import "./ProfilePage.css";
 
 
 const ProfilePage = () => {
@@ -19,6 +20,18 @@ const ProfilePage = () => {
             <h5>Pizza Time is a tool for restaurant owners to create their menu and sell their food. Pizza Time is also a tool for users to order food directly from the app. It was developed with Mongoose, Express, React, and Node. It is currently still a work in progress.</h5>
         </MDBRow>
       </MDBContainer> 
+      <br/>
+
+<center>
+  <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>Made with:</u></h3>
+      <ul>
+        <li>React</li>
+        <li>HTML/CSS/JS</li>
+        <li>Node/Express</li>
+        <li>MongoDB</li>
+        <li>JSON Web Tokens</li>
+      </ul>
+      </center>
     </div>
   );
 };
